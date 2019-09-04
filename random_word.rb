97643253c197e2b7e5e75cde813bee147a37bfee
Ian Clarkson <iclarkson@appcolony.ca>
2019-09-04 15:18:17 -0700

    Requesting gzip messes with RestClient (gets binary response)

--- random_word.rb
+++ random_word.rb
@@ -5,7 +5,6 @@ require 'rest-client'
 
 req_cookies = {
   "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  "Accept-Encoding" => "gzip, deflate",
   "Accept-Language" => "en-US,en;q=0.5",
   "Connection" => "keep-alive",
   "Host" => "www.oed.com",
